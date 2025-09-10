c01e9c7464021505ca1545257f81b8e9d1c755af
balikeshwar17 <balikeshwart@gmail.com>
2025-09-10 23:47:51 +0530

    changes pushing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,20 @@ import "../styles/Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="logo">NeuronWise</div>
-      <ul>
-        <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
-        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-        <li><Link to="products" smooth={true} duration={500}>Products</Link></li>
-        <li><Link to="applications" smooth={true} duration={500}>Applications</Link></li>
-        <li><Link to="careers" smooth={true} duration={500}>Careers</Link></li>
-        <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
-      </ul>
+      <div className="navbar-container">
+        <div className="logo">
+            NeuronWise
+        </div>
+
+        <ul>
+          <li><Link to="home" smooth={true} activeClass="active">Home</Link></li>
+          <li><Link to="about" smooth={true} activeClass="active">About</Link></li>
+          <li><Link to="products" smooth={true} activeClass="active">Products</Link></li>
+          <li><Link to="applications" smooth={true}  activeClass="active">Applications</Link></li>
+          <li><Link to="careers" smooth={true} activeClass="active">Careers</Link></li>
+          <li><Link to="contact" smooth={true} activeClass="active">Contact</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
